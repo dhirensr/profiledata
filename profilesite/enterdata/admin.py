1e362a5e4411fe8f123c7c40e723058f9624f179
dhirensr <dhirensr@gmail.com>
2018-04-30 01:23:46 +0530

    added functionality of the admin panel.

--- profilesite/enterdata/admin.py
+++ profilesite/enterdata/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import EnterData
+from django.contrib.auth.models import User
 # Register your models here.
 
-admin.site.register(EnterData)
+class EnterDataAdmin(admin.ModelAdmin):
+    def get_fields(self, request, obj=None):
+        fields = super(EnterDataAdmin, self).get_fields(request, obj)
+        if request.user.is_superuser:
+            fields= ('work_exp','german_grade','bachelors_percent')
+        else:
+            fields =('work_exp','german_grade')
+        return fields
+
+
+admin.site.register(EnterData,EnterDataAdmin)
