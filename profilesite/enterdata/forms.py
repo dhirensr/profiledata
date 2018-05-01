323f7f3cea22bda7e18637e3a4f2f547db67a0da
dhirensr <dhirensr@gmail.com>
2018-05-01 22:35:08 +0530

    added models change

--- profilesite/enterdata/forms.py
+++ profilesite/enterdata/forms.py
@@ -4,12 +4,14 @@ from .models import EnterData
 class NameForm(forms.ModelForm):
     class Meta:
         model=EnterData
-        fields=('work_exp',)
+        fields=('bachelors_stream','class_10_percent','class_12_percent',\
+                'bachelors_percent','work_exp','toefl','gre_score','admit_unis',\
+                'reject_unis','ms_stream','extra_curricular','name')
 
 class TestForm(forms.Form,forms.ModelForm):
     class Meta:
         model = EnterData
-        fields= ('work_exp','first_name')
+        fields= ('work_exp','name')
     hint1 = forms.CharField(max_length=30,required=True)
     hint2 = forms.CharField(max_length=30,required=True)
     hint3 = forms.CharField(max_length=30,required=True)
--- profilesite/enterdata/models.py
+++ profilesite/enterdata/models.py
@@ -3,16 +3,34 @@ from jsonfield import JSONField
 
 # Create your models here.
 class EnterData(models.Model):
-    first_name = models.CharField(max_length=50,blank=True)
-    last_name = models.CharField(max_length=50,blank=True)
-    university_name =models.CharField(max_length=200,blank=True)
-    class_10_percent = models.IntegerField(blank=True,default=0)
-    class_12_percent = models.IntegerField(blank=True,default=0)
-    bachelors_percent = models.IntegerField(blank=True,default=0)
-    work_exp = models.CharField(max_length=200)
-    german_grade = models.CharField(max_length=20,verbose_name="German Grade (For eg: A1,A2,etc)")
-    semester = models.CharField(max_length=200,blank = True)
-    hints = JSONField(default=[],blank = True)
+    name = models.CharField(max_length=50,blank=True,\
+                                  verbose_name='Your Name (only if you want to submit)')
+    admit_unis = models.TextField(max_length=400,\
+                                  verbose_name='Add all the Universities and course that you received ADMIT from')
+    reject_unis = models.TextField(max_length=400,blank=True,\
+                                   verbose_name='Add all the Universities and course that you received a REJECTION from')
+    ms_stream = models.CharField(max_length=400,\
+                                 verbose_name='MS Field (Which field are you studying in right now)')
+    extra_curricular= models.TextField(max_length=400,blank=True,\
+                                       verbose_name='Misc. (Some important extra curricular activities or anything that you think is important to have in the profile)')
+    class_10_percent = models.IntegerField(default=0,verbose_name='10th Grade percent( Rounded off)')
+    class_12_percent = models.IntegerField(default=0,\
+                                           verbose_name='12th/Diploma Grade	12th/Diploma Grade	12th/Diploma Grade (Rounded off)')
+    bachelors_percent = models.FloatField(default=0,\
+                                            verbose_name='B.Tech. score ')
+    bachelors_stream = models.CharField(max_length=100,default=0,\
+                                        verbose_name='B.Tech. stream')
+    toefl = models.IntegerField(default=0,\
+                                verbose_name='TOEFL/IELTS')
+    gre_score = models.IntegerField(blank=True,default=0,\
+                                    verbose_name='GRE score')
+    work_exp = models.IntegerField(default=0,blank=True,
+                                   verbose_name='Work Experience ')
+    german_grade = models.CharField(max_length=20,verbose_name="German Grade (For eg: A1,A2,etc)",\
+                                    blank=True)
+    semester = models.IntegerField(default=0,blank = True,\
+                                   verbose_name='Semester (Meaning how many semesters have you studied till now.)')
+    timestamp= models.DateTimeField(auto_now=True)
 
 
 class HintsData(models.Model):
